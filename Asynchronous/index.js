d3cd5be4ff6b0352c8e5c6bba81aebdf39e17f10
Harish Nishad <harish.nishad0775@gmail.com>
2022-02-22 00:10:15 +0530

    Basic Promises

--- Asynchronous/index.js
+++ Asynchronous/index.js
@@ -1,28 +1,72 @@
 
 
-console.log("Before");
+const myPromise = new Promise((resolve, reject) => {
+ // resolve(1);
 
- getuser(1,(user)=>{
-     console.log(user);
-     repo(user.githubUser,(repo)=>{
-         console.log("Repos",repo);
-     })
- });
+  reject(new Error('Reject Error message'));
+});
 
-function getuser(id,callback){  
-      console.log("Call User from db.......");
+myPromise.then(result=>console.log("result",result))
+.catch(err=>console.log("Err while Fatch",err.message));
 
-setTimeout(() => {
 
-    callback ({ id : id,githubUser: "Harish Nishad"});
-}, 4000);
 
-}
-//console.log("After");
 
-function repo(_username,callback){
-   console.log("Repositry is Fetching from Github");
-    setTimeout(() => {
-            callback(['repo1' , 'repo2' ,'repo3','repo4']);
-    }, 6000);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Before");
+
+//  getuser(1,(user)=>{
+//      console.log(user);
+//      repo(user.githubUser,(repo)=>{
+//          console.log("Repos",repo);
+//      })
+//  });
+
+// function getuser(id,callback){  
+//       console.log("Call User from db.......");
+
+// setTimeout(() => {
+
+//     callback ({ id : id,githubUser: "Harish Nishad"});
+// }, 4000);
+
+// }
+// //console.log("After");
+
+// function repo(_username,callback){
+//    console.log("Repositry is Fetching from Github");
+//     setTimeout(() => {
+//             callback(['repo1' , 'repo2' ,'repo3','repo4']);
+//     }, 6000); 
+// }
