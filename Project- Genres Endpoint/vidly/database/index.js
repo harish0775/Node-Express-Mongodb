84638258cea55963b854806d2a54c92b83b79e41
Harish Nishad <harish.nishad0775@gmail.com>
2022-02-23 01:07:07 +0530

    Create Schema

--- Project- Genres Endpoint/vidly/database/index.js
+++ Project- Genres Endpoint/vidly/database/index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/player')
 .then(()=>console.log('Connect to Database...'))
 .catch(err=>console.error("Failed to connnect db",err));
+
+const courseSchema = new mongoose.Schema({
+   name: String,
+   author: String,
+   tag : [String],
+   date : {type:Date,default:Date.now},
+   ispublish : Boolean
+})
