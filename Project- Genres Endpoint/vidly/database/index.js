1b0e065f7f4946cc98a8408635e1592184974ecd
Harish Nishad <harish.nishad0775@gmail.com>
2022-02-23 01:21:31 +0530

    Save Document in DataBase

--- Project- Genres Endpoint/vidly/database/index.js
+++ Project- Genres Endpoint/vidly/database/index.js
@@ -12,9 +12,15 @@ const courseSchema = new mongoose.Schema({
    ispublish : Boolean
 })
 const Course = mongoose.model('Course',courseSchema);
-const course = new Course({
+async function createCouse(){
+    const course = new Course({
  name : 'Node Node.js Course',
  author : 'Harish Nishad',
  tag : ['Backend','Node.js'],
  ispublish : true
 })
+
+const result = await course.save();
+console.log('result',result);
+}
+createCouse();
