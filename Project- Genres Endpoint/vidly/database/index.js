b29420b38d9f0fd10bff03de5496ef3ae46271f4
Harish Nishad <harish.nishad0775@gmail.com>
2022-02-23 01:07:47 +0530

    Create Model

--- Project- Genres Endpoint/vidly/database/index.js
+++ Project- Genres Endpoint/vidly/database/index.js
@@ -11,3 +11,10 @@ const courseSchema = new mongoose.Schema({
    date : {type:Date,default:Date.now},
    ispublish : Boolean
 })
+const Course = mongoose.model('Course',courseSchema);
+const course = new Course({
+ name : 'Node Node.js Course',
+ author : 'Harish Nishad',
+ tag : ['Backend','Node.js'],
+ ispublish : true
+})
